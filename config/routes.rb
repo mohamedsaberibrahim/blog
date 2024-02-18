eba72729572d86f6189a7c6feebad39704ad0ff1
Mohamed Saber <mohameds.meligi@gmail.com>
2024-02-18 14:08:32 +0200

    adding comments crud

--- config/routes.rb
+++ config/routes.rb
@@ -6,10 +6,9 @@ Rails.application.routes.draw do
 
   root "articles#index"
 
-  resources :articles
-  # get "/articles", to: "articles#index"
-  # get "/articles/:id", to: "articles#show"
-
+  resources :articles do
+    resources :comments
+  end
   # Defines the root path route ("/")
   # root "posts#index"
 end
